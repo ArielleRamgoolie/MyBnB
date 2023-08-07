be173e336df1eec0ce719dd2550694fb21a93b56
your_username <arielleramgoolie27@gmail.com>
2023-08-06 22:03:13 -0400

    updated search by price range filter

--- src/Search.java
+++ src/Search.java
@@ -12,6 +12,7 @@ public class Search {
     public static void ascendingPrices(Connection con) {
 
         String query = "SELECT * FROM Listings ORDER BY price ASC";
+        printListings(con, query);
 
     	try {
             Statement stmt = con.createStatement();
@@ -128,7 +129,7 @@ public class Search {
 
             // query listings 
             Statement stmt = con.createStatement();
-            String query = "SELECT id, host_id, type, price, address, latitude, longitude FROM Listings";
+            String query = "SELECT * FROM Listings";
             ResultSet rs = stmt.executeQuery(query);
 
             // search through all the results 
@@ -143,8 +144,7 @@ public class Search {
                     String hostId = rs.getString("host_id");
                     String type = rs.getString("type");
                     double price = rs.getDouble("price");
-                    String address = rs.getString("address");
-                    String listingProperties = String.format("%-10s %-10s %-10s %-10.2f %-20s %-10s %-10s", id, hostId, type, price, address, currListingLat, currListingLong);
+                    String listingProperties = String.format("%-10s %-10s %-10s %-10.2f %-20s %-10s %-10s", id, hostId, type, price, currListingLat, currListingLong);
                     results.add(listingProperties);
                 }
             }
@@ -154,7 +154,7 @@ public class Search {
             System.out.printf("All Listings");
             System.out.println();
             System.out.println("--------------------------------------------------------------------------------------");
-            System.out.printf("%-10s %-10s %-10s %-10s %-20s %-10s %-10s\n", "ListingID", "HostID", "Type", "Price","Address", "Latitude", "Longitude");
+            System.out.printf("%-10s %-10s %-10s %-10s %-10s %-10s\n", "ListingID", "HostID", "Type", "Price", "Latitude", "Longitude");
             System.out.println("--------------------------------------------------------------------------------------");
             for (String listing : results) {
                 System.out.println(listing);
@@ -185,71 +185,55 @@ public class Search {
     }
 
     public static void addressSerch(Connection con){
-        try{
-            System.out.println("Please enter the exact house number of the listing you would like to search for:");
-            int housenumber = sc.nextInt();
-            sc.nextLine();
-            System.out.println("Please enter the exact street name of the listing you would like to search for:");
-            String streetname = sc.nextLine();
-            System.out.println("Please enter the exact city name of the listing you would like to search for:");
-            String city = sc.nextLine();
-            System.out.println("Please enter the exact country name of the listing you would like to search for:");
-            String country = sc.nextLine();
+        System.out.println("Please enter the exact house number of the listing you would like to search for:");
+        int housenumber = sc.nextInt();
+        sc.nextLine();
+        System.out.println("Please enter the exact street name of the listing you would like to search for:");
+        String streetname = sc.nextLine();
+        System.out.println("Please enter the exact city name of the listing you would like to search for:");
+        String city = sc.nextLine();
+        System.out.println("Please enter the exact country name of the listing you would like to search for:");
+        String country = sc.nextLine();
 
-            // query listings 
-            String query = "SELECT * FROM Listings WHERE house_number = '" + housenumber + "' AND street_name = '" + streetname + "' AND city = '" + city + "' AND country = '" + country + "'"  ; 
-            Statement stmt = con.createStatement();
-            ResultSet rs = stmt.executeQuery(query);
+        // query listings 
+        String query = "SELECT * FROM Listings WHERE house_number = '" + housenumber + "' AND street_name = '" + streetname + "' AND city = '" + city + "' AND country = '" + country + "'"  ; 
 
-            App.clearScreen();
-            System.out.println("All Listings in Descending Prices:");
-            System.out.println("------------------------------------------------------------------------------------------------------------------------------");
-            System.out.printf("%-10s %-5s %-10s %-10s %-11s %-20s %-10s %-10s %-11s %-11s %-11s\n", "ListingID", "Host", "Type", "Price", "HouseNumber", "StreetName", "City", "Country", "PostalCode", "Latitude", "Longitude");
-            System.out.println("------------------------------------------------------------------------------------------------------------------------------");
-            
-            while (rs.next()) {
-                int host = rs.getInt("host_id");
-                int listing = rs.getInt("id");
-                String type = rs.getString("type");
-                float longitude = rs.getFloat("longitude");
-                float latitude = rs.getFloat("latitude");
-                float price = rs.getFloat("price");
-                housenumber = rs.getInt("house_number");
-                streetname = rs.getString("street_name");
-                city = rs.getString("city");
-                country = rs.getString("country");
-                String postalcode = rs.getString("postal_code");
+        printListings(con, query);
 
-                System.out.printf("%-10d %-5d %-10s %-10.2f %-11d %-20s %-10s %-10s %-11s %-11.3f %-11.3f\n", listing, host, type, price, housenumber, streetname, city, country, postalcode, latitude, longitude);
-            }
-            System.out.println("------------------------------------------------------------------------------------------------------------------------------");
-            rs.close();
-            stmt.close();
+        filter(con, query);
 
-            // Wait for user input (pressing Enter) before continuing
-            System.out.println("\nPress Enter to continue...");
-            sc.nextLine();
+        // Wait for user input (pressing Enter) before continuing
+        System.out.println("\nPress Enter to continue...");
+        sc.nextLine();
 
-            return;
-        
-        } catch (SQLException e) {
-            e.printStackTrace(); 
-        }
-    
+        return;
     }
 
     public static void postalCode(Connection con){
-        try{
-            System.out.println("Please enter the postal code you would like to search for:");
-            String pc = sc.nextLine();
+        System.out.println("Please enter the postal code you would like to search for:");
+        String pc = sc.nextLine();
+        
+        // query listings
+        String query = "SELECT * FROM Listings WHERE (postal_code = '" + pc + "' OR LEFT(postal_code, 2) = LEFT('" + pc +"', 2))"; 
+        printListings(con, query);
 
-            // query listings
-            String query = "SELECT * FROM Listings WHERE postal_code = '" + pc + "' OR LEFT(postal_code, 2) = LEFT('" + pc +"', 2)"; 
+        // ask if they would like to further filter the search
+        filter(con, query);
+
+        // Wait for user input (pressing Enter) before continuing
+        System.out.println("\nPress Enter to continue...");
+        sc.nextLine();
+
+        return;
+    
+    }
+
+    public static void printListings(Connection con, String query){
+        try {
             Statement stmt = con.createStatement();
             ResultSet rs = stmt.executeQuery(query);
-
             App.clearScreen();
-            System.out.println("All Listings in Descending Prices:");
+            System.out.println("Results:");
             System.out.println("------------------------------------------------------------------------------------------------------------------------------");
             System.out.printf("%-10s %-5s %-10s %-10s %-11s %-20s %-10s %-10s %-11s %-11s %-11s\n", "ListingID", "Host", "Type", "Price", "HouseNumber", "StreetName", "City", "Country", "PostalCode", "Latitude", "Longitude");
             System.out.println("------------------------------------------------------------------------------------------------------------------------------");
@@ -274,16 +258,26 @@ public class Search {
             rs.close();
             stmt.close();
 
-            // Wait for user input (pressing Enter) before continuing
-            System.out.println("\nPress Enter to continue...");
-            sc.nextLine();
-
-            return;
-        
         } catch (SQLException e) {
             e.printStackTrace(); 
         }
-    
+    }
+
+    public static void filter(Connection con, String query){
+        System.out.println("Would you like to further filter this search? (Y/N)");
+        if (sc.nextLine().equals("Y")){
+            System.out.println("What would you like to filter by? (eg. price range, amenities, availibility)");
+            if (sc.nextLine().equals("price range")){
+                System.out.println("Enter max price");
+                int max = sc.nextInt();
+                System.out.println("Enter min price");
+                int min = sc.nextInt();
+                query = query + " AND price BETWEEN " + min + " AND " + max;
+                printListings(con, query);
+                sc.nextLine();
+            }
+        }
+
     }
     
 }
